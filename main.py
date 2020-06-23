cbd14cd355c60d53147ec493ce10c8094bbee167
Bohdan Borysei <borysej90@gmail.com>
2020-06-23 21:52:58 +0300

    Signal handler message change

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 from bot import ReminderBot
 
 def signals_handler(sig, frame):
-    print("[INFO] You have pressed `Ctrl + C`, stopping the program...")
+    print(f"[INFO] Signal {sig} received, stopping the program...")
     
     bot.stop()
 
