ccb599e5d600a466de1d42e40ed670311d3dd231
Bohdan Borysei <borysej90@gmail.com>
2020-06-23 21:46:16 +0300

    Added SIGTERM

--- main.py
+++ main.py
@@ -6,14 +6,13 @@ import os
 
 from bot import ReminderBot
 
-
 def signals_handler(sig, frame):
     print("[INFO] You have pressed `Ctrl + C`, stopping the program...")
     
     bot.stop()
 
 print("[INFO] press Ctrl+C to stop the program...")
-signal.signal(signal.SIGINT, signals_handler)
+signal.signal(signal.SIGINT | signal.SIGTERM, signals_handler)
 
 bot = ReminderBot(os.environ["TOKEN"])
 bot.start()
