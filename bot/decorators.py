b29835c1ecaf5265c503412c8badb05487e40957
borysej90 <borysej90@gmail.com>
2020-08-02 18:20:28 +0300

    Modified decorators to accept self argument

--- bot/decorators.py
+++ bot/decorators.py
@@ -7,11 +7,11 @@ from .config import language as cfg
 
 def language(func):
     def wrapper(*args):
-        update = args[0]
-        context = args[1]
+        update = args[-2]
+        context = args[-1]
 
         if 'lang' in context.user_data:            
-            return func(update, context)
+            return func(*args)
         else:
             context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I don't know your language.\nPlease type /start")
     
@@ -19,8 +19,8 @@ def language(func):
 
 def time_format(func):
     def wrapper(*args):
-        update = args[0]
-        context = args[1]
+        update = args[-2]
+        context = args[-1]
 
         message = update.message.text
 
