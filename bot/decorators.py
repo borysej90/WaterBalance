b582b23d42ba9f6e7a85d4fe5cac6844a7dbc129
borysej90 <borysej90@gmail.com>
2020-08-02 14:17:24 +0300

    Fix import error

--- bot/decorators.py
+++ bot/decorators.py
@@ -3,7 +3,7 @@ import os
 from telegram import Update
 from telegram.ext import CallbackContext
 
-from config import language as cfg
+from .config import language as cfg
 
 def language(func):
     def wrapper(*args):
